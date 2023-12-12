cb988530580893c1a8edfb56d16bcc6545bf1ad4
bianavic <bianavic@gmail.com>
2023-12-12 11:17:10 -0300

    feat: create a testing endpoint

---
+++ src/main/java/org/edu/fabs/rdsapirest/HelloController.java
@@ -0,0 +1,16 @@
+package org.edu.fabs.rdsapirest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping
+    String getHello() {
+        return "Hello, Stranger!";
+    }
+
+}
