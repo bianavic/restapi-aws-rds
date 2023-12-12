d9ac54ebeed34cd6f7826023a4d4c8245841fb4f
bianavic <bianavic@gmail.com>
2023-12-12 08:36:22 -0300

    initialize project

---
+++ src/main/java/org/edu/fabs/rdsapirest/RdsapirestApplication.java
@@ -0,0 +1,13 @@
+package org.edu.fabs.rdsapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdsapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdsapirestApplication.class, args);
+	}
+
+}
